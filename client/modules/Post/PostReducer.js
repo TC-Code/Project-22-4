a6941daf0d58e90643554120f4a6b07d6be7f469
Tomasz Chowaniec <tomasz.chowaniec@icloud.com>
2018-06-08 15:36:14 +0200

    Add post editing

--- client/modules/Post/PostReducer.js
+++ client/modules/Post/PostReducer.js
@@ -1,23 +1,32 @@
-import { ADD_POST, ADD_POSTS, DELETE_POST } from './PostActions';
+import { ADD_POST, EDIT_POST, ADD_POSTS, DELETE_POST } from "./PostActions";
 
 // Initial State
 const initialState = { data: [] };
 
 const PostReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_POST :
+    case ADD_POST:
       return {
-        data: [action.post, ...state.data],
+        data: [action.post, ...state.data]
       };
 
-    case ADD_POSTS :
+    case EDIT_POST:
       return {
-        data: action.posts,
+        data: state.data.map(post => {
+          return post.cuid === action.cuid
+            ? Object.assign({}, post, action.post)
+            : post;
+        })
       };
 
-    case DELETE_POST :
+    case ADD_POSTS:
       return {
-        data: state.data.filter(post => post.cuid !== action.cuid),
+        data: action.posts
+      };
+
+    case DELETE_POST:
+      return {
+        data: state.data.filter(post => post.cuid !== action.cuid)
       };
 
     default:
@@ -31,7 +40,8 @@ const PostReducer = (state = initialState, action) => {
 export const getPosts = state => state.posts.data;
 
 // Get post by cuid
-export const getPost = (state, cuid) => state.posts.data.filter(post => post.cuid === cuid)[0];
+export const getPost = (state, cuid) =>
+  state.posts.data.filter(post => post.cuid === cuid)[0];
 
 // Export Reducer
 export default PostReducer;
